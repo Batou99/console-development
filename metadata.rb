a3d9ebfb92272c4c1b51586f5db8517124b50546
Lorenzo Lopez Sancho <lorenzo.lopez@intec.es>
2014-01-26 16:55:24 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'lorenzo.lopez@intec.es'
 license          'All rights reserved'
 description      'Installs/Configures console-development'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.2'
 depends          'apt'
 depends          'https://github.com/7even/oh-my-zsh'
 
